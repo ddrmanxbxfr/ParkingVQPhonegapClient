47c3035b7105e217e13187cd6f244125f307445a
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-08-30 20:08:53 -0400

    Updated hooks + permissions

--- platforms/android/assets/www/js/map_operations.js
+++ platforms/android/assets/www/js/map_operations.js
@@ -1,9 +1,31 @@
+/*jslint nomen: true*/
 /*global L,$,console*/
-var map, markers;
+var map, markers, locsLoadedInMemory;
+
+function evaluateIfIShouldLoadWaypointsFromApi(vs, swY, swX, neY, neX) {
+    "use strict";
+    var len, i, j, xj, yj, xi, yi;
+    if (locsLoadedInMemory !== undefined && locsLoadedInMemory.swX !== undefined && locsLoadedInMemory.swY !== undefined && locsLoadedInMemory.neX !== undefined && locsLoadedInMemory.neY !== undefined) {
+        len = vs.length;
+        for (i = 0, j = len - 1; i < len; j = i + 1) {
+            xi = locsLoadedInMemory.neX;
+            yi = locsLoadedInMemory.swX;
+            xj = locsLoadedInMemory.neY;
+            yj = locsLoadedInMemory.swY;
+            if ((neY > yi || swY > yj) &&
+                    (neX < xj || swX < xi)) {
+                return true;
+            }
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
 
 function clearWaypoints() {
     "use strict";
-    if (markers != undefined && markers !== null) {
+    if (markers !== undefined && markers !== null) {
         map.removeLayer(markers);
     }
 }
@@ -26,9 +48,26 @@ function trouverCenterFromBounds(h1, h2, b1, b2) {
     return point;
 }
 
+function mettreAJoursBoundsCache(latlngBounds) {
+    "use strict";
+    if (locsLoadedInMemory !== undefined) {
+        locsLoadedInMemory.swY = latlngBounds._southWest.lat;
+        locsLoadedInMemory.swX = latlngBounds._southWest.lng;
+        locsLoadedInMemory.neY = latlngBounds._northEast.lat;
+        locsLoadedInMemory.neX = latlngBounds._northEast.lng;
+    } else {
+        locsLoadedInMemory = {
+            swY: latlngBounds._southWest.lat,
+            swX: latlngBounds._southWest.lng,
+            neY: latlngBounds._northEast.lat,
+            neX: latlngBounds._northEast.lng
+        };
+    }
+}
+
 function ajouterWaypointALaMap(geojsonMarkers) {
     "use strict";
-    var progressBar, progress, markerList, lenFeatures, marker;
+    var progressBar, progress, markerList, lenFeatures, marker, i;
     clearWaypoints();
     progress = document.getElementById('progress');
     progressBar = document.getElementById('progress-bar');
@@ -45,11 +84,14 @@ function ajouterWaypointALaMap(geojsonMarkers) {
             progress.style.display = 'none';
         }
     }
-    markers = L.markerClusterGroup({ chunkedLoading: true, chunkProgress: updateProgressBar });
+    markers = L.markerClusterGroup({
+        chunkedLoading: true,
+        chunkProgress: updateProgressBar
+    });
     markerList = [];
     lenFeatures = geojsonMarkers.features.length;
-    for (var i = 0; i < lenFeatures; i++) {
-        var marker = L.marker(L.latLng(geojsonMarkers.features[i].geometry.coordinates[1], geojsonMarkers.features[i].geometry.coordinates[0]));
+    for (i = 0; i < lenFeatures; i + 1) {
+        marker = L.marker(L.latLng(geojsonMarkers.features[i].geometry.coordinates[1], geojsonMarkers.features[i].geometry.coordinates[0]));
         markerList.push(marker);
     }
     console.log('adding to layer : ' + markerList.length);
@@ -58,6 +100,7 @@ function ajouterWaypointALaMap(geojsonMarkers) {
 }
 
 function ajouterWaypointsBounds(latlngBounds) {
+    "use strict";
     var url, geojsonFeature, geoJsonToShow;
 
     geojsonFeature = new L.GeoJSON();
@@ -70,27 +113,7 @@ function ajouterWaypointsBounds(latlngBounds) {
             "name": data.name,
             "type": data.type
         };
-
-        ajouterWaypointALaMap(geoJsonToShow);
-    });
-};
-
-function ajouterWaypointsRadius(radiusTarget, latlngLocs) {
-    "use strict";
-    var geojsonFeature, geoJsonToShow, url, pointCentral;
-
-    pointCentral = trouverCenterFromBounds(latlngLocs._southWest.lat, latlngLocs._northEast.lat, latlngLocs._southWest.lng, latlngLocs._northEast.lng);
-    console.log('lat :' + pointCentral.lat + ' lng: ' + pointCentral.lng);
-    geojsonFeature = new L.GeoJSON();
-    geoJsonToShow = {};
-    url = "http://vps84512.ovh.net:4711/api/parking/" + radiusTarget + "/" + pointCentral.lat + "/" + pointCentral.lng;
-    //  console.log(url);
-    $.getJSON(url, function (data) {
-        geoJsonToShow = {
-            "features": data.features,
-            "name": data.name,
-            "type": data.type
-        };
+        mettreAJoursBoundsCache(latlngBounds);
         ajouterWaypointALaMap(geoJsonToShow);
     });
 }
@@ -131,8 +154,8 @@ function initMap() {
     // Methodes lorsque le user deplace la map...
     map.on("dragstart", clearWaypoints);
     map.on("dragend", refreshMap);
-   // map.on("zoomstart", clearWaypoints);
-//    map.on("zoomend", refreshMap);
+    map.on("zoomstart", clearWaypoints);
+    map.on("zoomend", refreshMap);
 
     // Trouve moi donc où je suis !
     map.locate({
