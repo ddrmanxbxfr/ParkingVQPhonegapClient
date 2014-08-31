8ac74724573f1e99d5824af2dae82667dd47498e
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-08-30 23:58:10 -0400

    southwest with southwest and northeast with northeast

--- www/js/map_operations.js
+++ www/js/map_operations.js
@@ -32,7 +32,7 @@ function updateLocsInMemory(latlngbounds) {
 function evaluateIfIShouldLoadWaypointsFromApi(mapBounds) {
     "use strict";
     if (isLocsLoadedInMemory()) {
-        if ((mapBounds._northEast.lng > locsLoadedInMemory.swX || mapBounds._southWest.lng > locsLoadedInMemory.swY) && (mapBounds._northEast.lat < locsLoadedInMemory.neY || mapBounds._southWest.lat < locsLoadedInMemory.neX)) {
+        if ((mapBounds._southWest.lng > locsLoadedInMemory.swX || mapBounds._southWest.lng > locsLoadedInMemory.swY) && (mapBounds._northEast.lat < locsLoadedInMemory.neY || mapBounds._northEast.lat < locsLoadedInMemory.neX)) {
             return false;
         } else {
             return true;
