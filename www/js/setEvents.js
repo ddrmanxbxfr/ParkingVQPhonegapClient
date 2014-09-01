71704ab0f19ba70a59d0ca024c24f97add82f17e
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-01 02:20:59 -0400

    added event to close legend

--- www/js/setEvents.js
+++ www/js/setEvents.js
@@ -6,12 +6,27 @@ function templateShowMap() {
     $('#main-content').html("<div id=\"progress\"><div id=\"progress-bar\"></div></div><div id=\"map\"></div><script>initMap();<\/script>");
 }
 
+function showOverlayLegend() {
+    "use strict";
+    var overlayToShow, cl;
+    overlayToShow = document.getElementById('overlay_legend');
+    cl = overlayToShow.classList;
+    if (cl.contains('off')) {
+        cl.remove('off');
+    }
+}
+
 $('#btnLegend').click(function () {
     "use strict";
-    templateAskParking();
+    showOverlayLegend();
 });
 
 $('#btnShowMap').click(function () {
     "use strict";
     templateShowMap();
+});
+
+$('#btnLegendClose').click(function() {
+    "use strict";
+    alert('closing legend');
 });
