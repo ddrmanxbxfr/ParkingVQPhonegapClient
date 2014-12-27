407391bead64b4fd4557aad41476b1d66184c555
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-12-27 16:21:53 -0500

    Updated license on specific files

--- www/js/map_events.js
+++ www/js/map_events.js
@@ -1,3 +1,20 @@
+/*
+This file is part of ParkingVQPhonegapClient.
+
+ParkingVQPhonegapClient is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ParkingVQPhonegapClient is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ParkingVQPhonegapClient.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*jslint nomen: true*/
 /*global L,$,console, clearWaypoints, ajouterWaypointsBounds,showOverlay,hideOverlay, refreshMap,reducedDataset,evaluateIfIShouldLoadWaypointsFromApi, ajouterWaypointsDelta, shouldILoadUsingDelta, configurerCssMap,
 doOnOrientationChange*/
